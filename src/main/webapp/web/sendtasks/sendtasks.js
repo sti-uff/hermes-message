f081aa63718dfccb2a1dba63275144bcf559a504
Daniel Castellani <dancastellani@gmail.com>
2013-09-13 18:01:39 -0300

    (refs #5) added sendtasks list

--- src/main/webapp/web/sendtasks/sendtasks.js
+++ src/main/webapp/web/sendtasks/sendtasks.js
@@ -6,3 +6,16 @@ function ListCtrl($scope, $http) {
         $scope.sendtasks = data;
     });
 }
+
+function NewCtrl($scope) {
+    $scope.save = function(sendtask) {
+        $http.post('http://localhost:8080/hermes/api/sendtasks/', sendtask).
+                success(function(data) {
+            $scope.sendtasks = data;
+        });
+    };
+
+    $scope.reset = function() {
+        $scope.user = angular.copy($scope.master);
+    };
+}
